65dae408d102ffb0551a0f98584c045b4881f6d3
Shayan Akhtar <shayan0786@users.noreply.github.com>
2026-05-18 00:08:43 +0530

    Updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,23 @@
-import { useState } from "react";
+import './App.css'
 
-export default function App() {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const addTask = () => {
-    if (!task.trim()) return;
-    setTodos([...todos, { text: task, done: false }]);
-    setTask("");
-  };
-
-  const toggleTask = (index) => {
-    const updated = [...todos];
-    updated[index].done = !updated[index].done;
-    setTodos(updated);
-  };
-
-  const deleteTask = (index) => {
-    setTodos(todos.filter((_, i) => i !== index));
-  };
-
+function App() {
   return (
-    <div style={styles.container}>
-      <h1>Todo Application 1</h1>
+    <div className="container">
+      <div className="card">
+        <h1>🚀 React DevOps Project</h1>
 
-      <div style={styles.inputBox}>
-        <input
-          type="text"
-          placeholder="Enter a task..."
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          style={styles.input}
-        />
-        <button onClick={addTask} style={styles.addBtn}>
-          Add Button
-        </button>
+        <p>
+          Successfully deployed using
+          <span> AWS EC2 </span>,
+          <span> Docker </span>,
+          <span> Jenkins </span> &
+          <span> GitHub CI/CD </span>
+        </p>
+
+        <button>Deployment Successful 🎉</button>
       </div>
-
-      <ul style={styles.list}>
-        {todos.map((todo, index) => (
-          <li key={index} style={styles.item}>
-            <span
-              onClick={() => toggleTask(index)}
-              style={{
-                ...styles.text,
-                textDecoration: todo.done ? "line-through" : "none",
-              }}
-            >
-              {todo.text}
-            </span>
-
-            <button onClick={() => deleteTask(index)} style={styles.deleteBtn}>
-              ❌
-            </button>
-          </li>
-        ))}
-      </ul>
     </div>
-  );
+  )
 }
 
-const styles = {
-  container: {
-    textAlign: "center",
-    marginTop: "60px",
-    fontFamily: "sans-serif",
-  },
-  inputBox: {
-    marginBottom: "20px",
-  },
-  input: {
-    padding: "10px",
-    width: "220px",
-    marginRight: "10px",
-  },
-  addBtn: {
-    padding: "10px 15px",
-    cursor: "pointer",
-  },
-  list: {
-    listStyle: "none",
-    padding: 0,
-  },
-  item: {
-    width: "300px",
-    margin: "10px auto",
-    padding: "10px",
-    border: "1px solid #ccc",
-    display: "flex",
-    justifyContent: "space-between",
-    borderRadius: "6px",
-  },
-  text: {
-    cursor: "pointer",
-  },
-  deleteBtn: {
-    background: "transparent",
-    border: "none",
-    cursor: "pointer",
-  },
-};
+export default App

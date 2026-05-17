4cba3537d185205daebdde7eb91228d405b683b9
Shayan Akhtar <shayan0786@users.noreply.github.com>
2026-05-18 00:18:31 +0530

    Created Weather App

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,71 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [city, setCity] = useState("")
+  const [weather, setWeather] = useState(null)
+
+  const getWeather = async () => {
+
+    if(city === ""){
+      alert("Please enter city name")
+      return
+    }
+
+    const apiKey = "dbeffeef00f4d636f1c50ffa17892d37"
+
+    const url =
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setWeather(data)
+  }
+
   return (
-    <div className="main">
+    <div className="container">
 
-      <nav className="navbar">
-        <h2>⚡ DevOpsFlow</h2>
+      <div className="weather-card">
 
-        <div className="links">
-          <a href="/">Home</a>
-          <a href="/">Pipeline</a>
-          <a href="/">Docker</a>
-          <a href="/">AWS</a>
-        </div>
-      </nav>
+        <h1>🌦 Weather App</h1>
 
-      <div className="hero">
+        <div className="search-box">
 
-        <div className="left">
-          <h1>
-            CI/CD Pipeline <br />
-            using Jenkins & Docker 🚀
-          </h1>
+          <input
+            type="text"
+            placeholder="Enter city name"
+            onChange={(e)=>setCity(e.target.value)}
+          />
 
-          <p>
-            Modern DevOps deployment workflow using
-            React, GitHub, Jenkins, Docker and AWS EC2.
-          </p>
+          <button onClick={getWeather}>
+            Search
+          </button>
 
-          <button>Deployment Successful</button>
         </div>
 
-        <div className="right">
-          <div className="card">
-            <h3>⚙️ Tech Stack</h3>
+        {
+          weather && weather.main && (
 
-            <ul>
-              <li>React JS</li>
-              <li>Docker</li>
-              <li>Jenkins</li>
-              <li>AWS EC2</li>
-              <li>GitHub</li>
-            </ul>
-          </div>
-        </div>
+            <div className="weather-info">
+
+              <h2>{weather.name}</h2>
+
+              <h3>{weather.main.temp}°C</h3>
+
+              <p>🌡 Humidity : {weather.main.humidity}%</p>
+
+              <p>💨 Wind Speed : {weather.wind.speed} km/h</p>
+
+              <p>
+                ☁ Condition : {weather.weather[0].main}
+              </p>
+
+            </div>
+
+          )
+        }
 
       </div>
 

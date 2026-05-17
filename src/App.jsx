ece03862859fb74e3b0ba03900a0512ac3b6e078
Shayan Akhtar <shayan0786@users.noreply.github.com>
2026-05-18 00:11:40 +0530

    Created modern DevOps landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,51 @@ import './App.css'
 
 function App() {
   return (
-    <div className="container">
-      <div className="card">
-        <h1>🚀 React DevOps Project</h1>
+    <div className="main">
 
-        <p>
-          Successfully deployed using
-          <span> AWS EC2 </span>,
-          <span> Docker </span>,
-          <span> Jenkins </span> &
-          <span> GitHub CI/CD </span>
-        </p>
+      <nav className="navbar">
+        <h2>⚡ DevOpsFlow</h2>
 
-        <button>Deployment Successful 🎉</button>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/">Pipeline</a>
+          <a href="/">Docker</a>
+          <a href="/">AWS</a>
+        </div>
+      </nav>
+
+      <div className="hero">
+
+        <div className="left">
+          <h1>
+            CI/CD Pipeline <br />
+            using Jenkins & Docker 🚀
+          </h1>
+
+          <p>
+            Modern DevOps deployment workflow using
+            React, GitHub, Jenkins, Docker and AWS EC2.
+          </p>
+
+          <button>Deployment Successful</button>
+        </div>
+
+        <div className="right">
+          <div className="card">
+            <h3>⚙️ Tech Stack</h3>
+
+            <ul>
+              <li>React JS</li>
+              <li>Docker</li>
+              <li>Jenkins</li>
+              <li>AWS EC2</li>
+              <li>GitHub</li>
+            </ul>
+          </div>
+        </div>
+
       </div>
+
     </div>
   )
 }
